15fa0b57f68bd2be93c4f2aaff9a5f6dfd3721a5
Yonghui Zhang <68320372+maxcheung5259@users.noreply.github.com>
2021-01-09 19:46:23 +0800

    Initial Structure

---
+++ api/users/index.js
@@ -0,0 +1,120 @@
+import express from 'express';
+import User from './userModel';
+import jwt from 'jsonwebtoken';
+const router = express.Router(); // eslint-disable-line
+// Get all users
+router.get('/', (req, res,next) => {
+    User.find().then(users =>  res.status(200).json(users)).catch(next);
+});
+// Register OR authenticate a user
+router.post('/', async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({
+      success: false,
+      msg: 'Please pass username and password.',
+    });
+  }
+  if (req.query.action === 'register') {
+    await User.create(req.body).catch(next);
+    res.status(201).json({
+      code: 201,
+      msg: 'Successful created new user.',
+    });
+  } else {
+    const user = await User.findByUserName(req.body.username).catch(next);
+      if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          const token = jwt.sign(user.username, process.env.SECRET);
+          // return the information including token as JSON
+          res.status(200).json({
+            success: true,
+            token: 'BEARER ' + token,
+          });
+        } else {
+          res.status(401).json({
+            code: 401,
+            msg: 'Authentication failed. Wrong password.'
+          });
+        }
+      });
+    }
+});
+// Update a user
+router.put('/:id',  (req, res,next) => {
+    if (req.body._id) delete req.body._id;
+     User.update({
+      _id: req.params.id,
+    }, req.body, {
+      upsert: false,
+    })
+    .then(user => res.json(200, user)).catch(next);
+});
+//Add a favourite. No Error Handling Yet. Can add duplicates too!
+router.post('/:userName/favourites', async (req, res, next) => {
+  try{
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    if (user.favourites.indexOf(movie._id) === -1) {
+      await user.favourites.push(movie._id);
+    } else {
+      res.status(201).json({
+        msg: "This movie is already in favourites.",
+        user
+      });
+    }
+    await user.save(); 
+    res.status(201).json(user); 
+  } catch(err) {
+    next(err);
+  }
+});
+
+// Register OR authenticate a user
+router.post('/', async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(401).json({
+      success: false,
+      msg: 'Please pass username and password.',
+    });
+  }
+  if (req.query.action === 'register') {
+    const Pwd = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+    if (!req.body.password.match(Pwd)) {
+      res.status(401).json({
+        code: 401,
+        msg: 'Registeration failed,bad password.'
+      });
+    } else {
+      await User.create(req.body).catch(next);
+      res.status(201).json({
+        code: 201,
+        msg: 'Successfully created new user.',
+      });
+    }
+  } else {
+    const user = await User.findByUserName(req.body.username).catch(next);
+      if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          const token = jwt.sign(user.username, process.env.SECRET);
+          // return the information including token as JSON
+          res.status(200).json({
+            success: true,
+            token: 'BEARER ' + token,
+          });
+        } else {
+          res.status(401).json({
+            code: 401,
+            msg: 'Authentication failed. Wrong password.'
+          });
+        }
+      });
+    }
+});
+
+export default router;
